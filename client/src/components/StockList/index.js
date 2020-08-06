0db5eb703a443da6a17f49a120c698edbeeed01d
Wub A <ayele.wub@gmail.com>
2020-08-05 21:13:24 -0400

    updated styling for dashboard

--- client/src/components/StockList/index.js
+++ client/src/components/StockList/index.js
@@ -172,10 +172,10 @@ function StockList(props) {
           </ButtonGroup>
         </Col>
         <Col lg="9" xs="12" md="9" s="12">
-          <Jumbotron>
+          <Jumbotron id="chartCont">
             <div className="chart">
               {chartData[0] && (
-                <Line
+                <Line id="chart"
                   data={{
                     datasets: chartData.map((c) => ({
                       label: c.symbol,
